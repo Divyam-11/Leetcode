f3f1b32a03efe80d21b756066adbc71fea096e06
Divyam-11 <83936994+Divyam-11@users.noreply.github.com>
2024-02-26 21:29:24 +0530

    Time: 26 ms (79.31%) | Memory: 12.1 MB (91.51%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,35 +1,27 @@
 class Solution {
-private:
-int solve(vector<int> &coins,int target,int index,vector<vector<int>> &dp){
-if(target == 0){
-    return 0;
-}
-if(index < 0){
-    return INT_MAX;
-}
-if(dp[target][index] != -1){
-    return dp[target][index];
-}
-
-int take = INT_MAX;
-if(target >= coins[index] && solve(coins,target - coins[index],index,dp) != INT_MAX){
-    take = 1 + solve(coins,target-coins[index],index,dp);
-}
-int not_take = solve(coins,target,index-1,dp);
-dp[target][index] = min(not_take,take);
-return dp[target][index];
-
-
-
-}
-// pick answer 
-// dont pick
-// return minimum
-
-public:
+public:    
+    int dp[12 + 1][10000 + 1];
+    
+    int findLowestCoins(vector<int> &coins, int arraySize, int amount) {
+        for (int i = 0; i < arraySize + 1; i++)
+            for (int j = 0; j < amount + 1; j++)
+                if (i == 0 || j == 0)
+                    dp[i][j] = (j == 0) ? 0 : INT_MAX - 1;
+        
+        for (int i = 1; i < arraySize + 1; i++) {
+            for (int j = 1; j < amount + 1; j++) {
+                if (coins[i - 1] > j)
+                    dp[i][j] = 0 + dp[i - 1][j];
+                else
+                    dp[i][j] = min(0 + dp[i - 1][j], 1 + dp[i][j - coins[i - 1]]);
+            }
+        }
+        
+        return dp[arraySize][amount];
+    }
+    
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> dp(amount+1,vector<int>(coins.size()+1,-1));
-        int result =  solve(coins,amount,coins.size( ) - 1,dp);
-        return result == INT_MAX ? -1 : result;
+        int res = findLowestCoins(coins, coins.size(), amount);
+        return (res == INT_MAX - 1) ? -1 : res;
     }
 };
