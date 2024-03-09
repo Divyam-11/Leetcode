23e5fce72d13eb877a44344a4a30c1d055af2dd4
Divyam-11 <83936994+Divyam-11@users.noreply.github.com>
2024-03-09 10:49:12 +0530

    Time: 30 ms (99.33%) | Memory: 52.5 MB (86.28%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-    unordered_map<int,int> m;
-    for(int i : nums1){
-    m[i]++;
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        int i=0,j=0;
+        while(i<nums1.size() && j<nums2.size()){
+            if(nums1[i]==nums2[j])return nums1[i];
+            else if(nums1[i]<nums2[j])i++;
+            else j++;
+        }
+        return -1;
     }
-    for(int i : nums2){
-      if(m.find(i) != m.end()){
-      return i;
-      }
-    }
-    return -1;
-}
 };
