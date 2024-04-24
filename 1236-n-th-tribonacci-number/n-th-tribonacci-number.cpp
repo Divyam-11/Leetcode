1c889940ecb38369a04975e4c9ccf10547a7ea12
Divyam-11 <83936994+Divyam-11@users.noreply.github.com>
2024-04-24 19:52:28 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (81.41%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,15 +1,16 @@
-class Solution {
-public:
-    int solve(int n,vector<int> &dp) {
-        if(n == 0) return 0;
-        if(n<=2) return 1;
-        if(dp[n] !=  -1 ) return dp[n];
-        return dp[n] = solve(n-1,dp) + solve(n-2,dp) + solve(n-3,dp);
-        
-    }
-        int tribonacci(int n) {
-            vector<int> dp(n+1,-1);
-            return solve(n,dp);
-        
+class Solution{ 
+    public:
+    int tribonacci(int n) {
+        if(n < 2)
+            return n;
+        int a1 = 0, a2 = 1, a3 = 1, t;
+        while(n > 2) {
+            t = a1 + a2 + a3;
+            a1 = a2;
+            a2 = a3;
+            a3 = t;
+            n--;
+        }
+        return a3;
     }
 };
