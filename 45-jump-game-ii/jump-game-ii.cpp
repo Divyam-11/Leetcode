bfd4c112b297bceeda9b0b063b7ee6afba674855
Divyam-11 <83936994+Divyam-11@users.noreply.github.com>
2024-06-18 21:32:29 +0530

    Time: 16 ms (40.34%) | Memory: 18.9 MB (87.44%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,23 +1,18 @@
 class Solution {
-    int func(vector<int> &nums,int index,int jumps,vector<int> &dp){
-        if(index>=nums.size()){
-            return 0;
-        }
-        else if(index == nums.size()-1){
-            return 0;
-        }
-        else if(dp[index] != -1) return dp[index];
-        int result = 10e5;
-        for(int i = 1 ; i<=nums[index];i++){
-        
-        result = min(result,1 + func(nums,index+i,jumps,dp));
-        }
-        return dp[index] = result;
-    }
 public:
     int jump(vector<int>& nums) {
-        int jumps = 0;
-        vector<int> dp(nums.size(),-1);
-        return func(nums,0,jumps,dp);
+        int res = 0;
+        int l = 0;
+        int r = 0;
+        while (r < nums.size() - 1) {
+            int farthest = 0;
+            for (int i = l; i <= r; ++i) {
+                farthest = max(farthest, i + nums[i]);
+            }
+            l = r + 1;
+            r = farthest;
+            res++;
+        }
+        return res;
     }
 };
