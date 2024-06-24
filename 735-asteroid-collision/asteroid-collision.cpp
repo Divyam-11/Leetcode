2f3a8383d763dc54e929b75a3eddba3623be94c6
Divyam-11 <83936994+Divyam-11@users.noreply.github.com>
2024-06-24 13:54:07 +0530

    Time: 13 ms (30.05%) | Memory: 20 MB (86.48%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> result;
+        for(int i = 0 ;i < asteroids.size();i++){
+            if(asteroids[i] > 0){
+                result.push_back(asteroids[i]);
+            }
+            else{
+                while(!result.empty() && result.back() > 0  && result.back() <  abs(asteroids[i])){
+                    result.pop_back();
+                }
+                if(result.empty()|| result.back( )<0){
+                    result.push_back(asteroids[i]);
+                }
+                else if(result.back() == abs(asteroids[i])){
+                    result.pop_back();
+                }  
+            }
+
+
+
+        
+        }
+        return result;
+    }
+};
