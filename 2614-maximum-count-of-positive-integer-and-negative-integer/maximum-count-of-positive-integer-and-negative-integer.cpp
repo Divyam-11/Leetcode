7b24720bcb6dc0cb58b5bf6bd073a5a270a3f568
Neo <83936994+Divyam-11@users.noreply.github.com>
2025-03-12 10:39:55 +0530

    Time: 0 ms (100.00%) | Memory: 21.5 MB (43.55%) - LeetSync

---
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+  int maximumCount(vector<int> &nums)
+  {
+    int negative_count = 0;
+    int positive_count = 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+      if (nums[i] > 0)
+        positive_count++;
+      else if (nums[i] < 0)
+        negative_count++;
+    }
+    return max(positive_count, negative_count);
+  }
+};
