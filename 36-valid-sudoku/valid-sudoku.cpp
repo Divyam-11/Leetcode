7095506eadf54f2f7a866e2653e1ef549df99b4f
Neo <83936994+Divyam-11@users.noreply.github.com>
2025-03-28 19:04:29 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (86.94%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,49 +1,26 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        unordered_map<char, int> freq;
+        bool col[9][9] = {false};
+        bool row[9][9] = {false};
+        bool box[9][9] = {false};
 
-        // Check rows
-        for (int i = 0; i < 9; i++) {
-            freq.clear();
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] == '.') continue;
-                if (freq.find(board[i][j]) != freq.end()) {
-                    return false;
-                } else {
-                    freq[board[i][j]]++;
+        for (int i = 0; i < 9; i++){
+            for (int j = 0; j < 9; j++){
+                if (board[i][j] == '.'){
+                    continue;
                 }
+                int num = board[i][j] - '0' - 1;
+                int ind = ((i / 3) * 3) + (j / 3);
+
+                if (col[i][num] || row[j][num] || box[ind][num]){
+                    return false;
+                }
+                col[i][num] = true;
+                row[j][num] = true;
+                box[ind][num] = true;
             }
         }
-
-        // Check columns
-        for (int j = 0; j < 9; j++) {
-            freq.clear();
-            for (int i = 0; i < 9; i++) {
-                if (board[i][j] == '.') continue;
-                if (freq.find(board[i][j]) != freq.end()) {
-                    return false;
-                } else {
-                    freq[board[i][j]]++;
-                }
-            }
-        }
-
-        // Check 3x3 subgrids using a 2D array of unordered_set
-        unordered_set<char> subgrid_freq[3][3];
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] == '.') continue;
-                int subgrid_row = i / 3;
-                int subgrid_col = j / 3;
-                if (subgrid_freq[subgrid_row][subgrid_col].find(board[i][j]) != subgrid_freq[subgrid_row][subgrid_col].end()) {
-                    return false;
-                } else {
-                    subgrid_freq[subgrid_row][subgrid_col].insert(board[i][j]);
-                }
-            }
-        }
-
         return true;
     }
 };
