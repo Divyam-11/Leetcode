0b9f703bc3fa5f45fb03bacd257fb0f5ecbfe66e
Neo <83936994+Divyam-11@users.noreply.github.com>
2025-04-04 15:16:12 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (45.12%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -5,6 +5,21 @@ public:
     {
         if (!root)
             return 0;
-       return max(1 + maxDepth(root->left), 1 + maxDepth(root->right));
+        return max(1 + maxDepth(root->left), 1 + maxDepth(root->right));
+    }
+    TreeNode *lcaDeepestLeaves(TreeNode *root)
+    {
+        if (!root)
+            return nullptr;
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+        if (left == right)
+            return root;
+        else if (left > right)
+            return lcaDeepestLeaves(root->left);
+        else
+            return lcaDeepestLeaves(root->right);
+
+        return nullptr;
     }
 };
