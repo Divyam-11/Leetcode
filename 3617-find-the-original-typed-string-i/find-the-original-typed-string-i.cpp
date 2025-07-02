d708d2eb15135ba10ebdb4762cf7d4fd1e1b0f81
Neo <83936994+Divyam-11@users.noreply.github.com>
2025-07-02 20:05:14 +0530

    Time: 6 ms (8.45%) | Memory: 8.9 MB (59.72%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
@@ -4,13 +4,19 @@ public:
     int possibleStringCount(string word)
     {
         int ans = 1;
+        int same_count = 0;
         for (int i = 1; i < word.size(); i++)
         {
             if (word[i] == word[i - 1])
             {
-                ans++;
+                same_count++;
+            }
+            else{
+                ans+=same_count;
+                same_count = 0;
             }
         }
+        ans+=same_count;
         return ans;
     }
 };
