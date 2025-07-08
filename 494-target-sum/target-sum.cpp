f60bba85f4bc5cea04d385623b38a89856e633ba
Neo <83936994+Divyam-11@users.noreply.github.com>
2025-07-08 13:12:23 +0530

    Time: 3 ms (87.93%) | Memory: 15.7 MB (42.90%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,26 +1,34 @@
 class Solution {
-private:
-int solve(vector<int> &nums,int target,int index)
-{
-if(index == -1 ){
-if(target == 0) return 1;
-else return 0;
-
-}
-
-int one = solve(nums,target - nums[index],index-1);
-int two = solve(nums,target + nums[index],index-1);
-
-
-
-return one + two;
-
-
-
-
-}
 public:
+    int perfectSum(vector<int> &arr, int sum) {
+        int n = arr.size();
+        vector<vector<int>> dp(n + 1, vector<int>(sum + 1, 0));
+
+        dp[0][0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= sum; j++) {
+                int pick = 0;
+                if (j >= arr[i - 1])
+                    pick = dp[i - 1][j - arr[i - 1]];
+                int not_pick = dp[i - 1][j];
+                dp[i][j] = pick + not_pick;
+            }
+        }
+
+        return dp[n][sum];
+    }
+
     int findTargetSumWays(vector<int>& nums, int target) {
-      return solve(nums,target,nums.size()-1);  
+        int totalSum = 0;
+        for (int num : nums)
+            totalSum += num;
+
+        if ((target + totalSum) % 2 != 0 || totalSum < abs(target))
+            return 0;
+
+        int s1 = (target + totalSum) / 2;
+
+        return perfectSum(nums, s1);
     }
 };
