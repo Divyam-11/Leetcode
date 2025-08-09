1aa4b09c7d8e1f00ed21113ef2d06bb4ccb786dd
Neo <83936994+Divyam-11@users.noreply.github.com>
2025-08-09 11:41:14 +0530

    Time: 7 ms (0.37%) | Memory: 7.9 MB (52.81%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n == 0) return false;
+        while(n){
+            cout<<n<<endl;
+            if(n % 2 !=0 && n != 1) return false;
+            n = n / 2; 
+        }
+        return true;
+    }
+};
