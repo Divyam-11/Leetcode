418dbcb23318370532c76478d4c698b4d3b7c905
Neo <83936994+Divyam-11@users.noreply.github.com>
2025-08-10 11:22:23 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (17.69%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,12 +1,48 @@
-class Solution {
+class Solution
+{
 public:
-    bool isPowerOfTwo(int n) {
-        if(n == 0) return false;
-        while(n){
-            cout<<n<<endl;
-            if(n % 2 !=0 && n != 1) return false;
-            n = n / 2; 
+    bool answer = false;
+    bool isPowerOfTwo(int n)
+    {
+        if (n <= 0)
+            return false;
+        return !(n & (n - 1));
+    }
+    void solve(unordered_map<int, int> &freq_map, string &number)
+    {
+        if (answer == true)
+            return;
+        if (freq_map.empty())
+        {
+            if (isPowerOfTwo(stoi(number)))
+                answer = true;
         }
-        return true;
+        for (auto &i : freq_map)
+        {
+            if (i.first == 0 && number.empty())
+                continue;
+            number.push_back(i.first + '0');
+            i.second--;
+            if (i.second == 0)
+            {
+                freq_map.erase(i.first);
+            }
+            solve(freq_map, number);
+            number.pop_back();
+            freq_map[i.first]++;
+        }
+    }
+
+    bool reorderedPowerOf2(int n)
+    {
+        unordered_map<int, int> freq;
+        string number = to_string(n);
+        for (int i = 0; i < number.size(); i++)
+        {
+            freq[number[i] - '0']++;
+        }
+        number = "";
+        solve(freq, number);
+        return answer;
     }
 };
