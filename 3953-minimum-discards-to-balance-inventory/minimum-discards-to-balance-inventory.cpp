6857fcdca995b94fbebab48bbf1d96a40bd67c9c
Neo <83936994+Divyam-11@users.noreply.github.com>
2025-09-14 12:53:18 +0530

    Time: 79 ms (100.00%) | Memory: 134.8 MB (100.00%) - LeetSync

---
+++ 3953-minimum-discards-to-balance-inventory/minimum-discards-to-balance-inventory.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+public:
+    int minArrivalsToDiscard(vector<int> &arrivals, int w, int m)
+    {
+        int discard = 0;
+        unordered_map<int, int> mp;
+        int l = 0;
+        for (int i = 0; i < arrivals.size(); i++)
+        {
+            
+
+            int item = arrivals[i];
+            if (mp[item] == m)
+            { 
+                discard++;
+                arrivals[i] = 0;
+            }
+            else
+            {
+                mp[item]++;
+            }
+                        int window = i - l + 1;
+            
+            if (window == w)
+            { 
+                mp[arrivals[l]]--; 
+                l++;
+                
+            }
+        }
+        return discard;
+    }
+};
