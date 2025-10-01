851d70db4935ffb662b8204e58b79d48b9f0ee75
Neo <83936994+Divyam-11@users.noreply.github.com>
2025-10-02 01:38:34 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (98.58%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+      int result = 0;
+      int emptyBottles = 0;
+      while(numBottles){
+       result+=numBottles;
+       emptyBottles += numBottles;
+       numBottles = emptyBottles / numExchange;
+       emptyBottles -= numBottles * numExchange;
+
+      }  
+      return result;
+    }
+};
