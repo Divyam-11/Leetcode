feccbd8e136457554deec3c3173f1905ae4bb779
Neo <83936994+Divyam-11@users.noreply.github.com>
2025-10-10 14:16:36 +0530

    Time: 280 ms (5.28%) | Memory: 151.8 MB (24.30%) - LeetSync

---
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    int maximumEnergy(vector<int> &energy, int k)
+    {
+        vector<int> dp(energy.size());
+        for (int i = 0; i < k; i++)
+        {
+            dp[i] = energy[i];
+        }
+        for (int i = k; i < energy.size(); i++)
+        {
+            // can take prev or kinda new start
+            dp[i] = max(energy[i], energy[i] + dp[i - k]);
+        }
+        int res = INT_MIN;
+        for (int i = energy.size() - 1; k>0; i--)
+        {   cout<<dp[i]<<endl;
+            res = max(res, dp[i]);
+            k--;
+        }
+        return res;
+    }
+};
