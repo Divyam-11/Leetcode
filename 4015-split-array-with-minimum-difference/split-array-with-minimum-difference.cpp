230faa4d5fd7b4a2e8c364bfc32e7d47b4e2206c
Neo <83936994+Divyam-11@users.noreply.github.com>
2025-10-13 22:41:25 +0530

    Time: 2820 ms (5.88%) | Memory: 184.9 MB (35.50%) - LeetSync

---
+++ 4015-split-array-with-minimum-difference/split-array-with-minimum-difference.cpp
@@ -0,0 +1,45 @@
+class Solution
+{
+public:
+    long long splitArray(vector<int> &nums)
+    {
+        vector<long long> prefix_sum(nums.size());
+        vector<bool> isLeft(nums.size(), false);
+        vector<bool> isRight(nums.size(), false);
+        isLeft[0] = true;
+        isRight[isRight.size() - 1] = true;
+        prefix_sum[0] = nums[0];
+        for (int i = 1; i < nums.size(); i++)
+        {
+            prefix_sum[i] = prefix_sum[i - 1] + nums[i];
+            if (nums[i] > nums[i - 1] && isLeft[i - 1])
+                isLeft[i] = true;
+        }
+        cout<<endl;
+        long long t_sum = prefix_sum.back();
+        for (int i = nums.size() - 2; i >= 0; i--)
+        {
+            if (nums[i + 1] < nums[i] && isRight[i + 1])
+            {
+                isRight[i] = true;
+            }
+            cout<<isRight[i]<<" ";
+        }
+        cout<<endl;
+        long long result = LLONG_MAX;
+
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            if (isLeft[i] && isRight[i + 1])
+            {   
+
+                cout<<isLeft[i]<<" "<<isRight[i+1]<<" "<<prefix_sum[i]<<endl;
+                long long temp = abs(t_sum - prefix_sum[i] - prefix_sum[i]);
+
+                result = min(result, temp);
+            }
+        }
+        
+        return (result == LLONG_MAX) ? -1:result;
+    }
+};
