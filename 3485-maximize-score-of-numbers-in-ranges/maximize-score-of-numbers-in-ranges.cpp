cfadffbfa8d874e8cb20fb027ae6edb1d12b3dde
Neo <83936994+Divyam-11@users.noreply.github.com>
2025-10-28 17:00:19 +0530

    Time: 67 ms (41.29%) | Memory: 109.7 MB (20.40%) - LeetSync

--- 3485-maximize-score-of-numbers-in-ranges/maximize-score-of-numbers-in-ranges.cpp
+++ 3485-maximize-score-of-numbers-in-ranges/maximize-score-of-numbers-in-ranges.cpp
@@ -1,50 +1,37 @@
-class Solution {
+class Solution
+{
 public:
-    bool canPlace(int mid, int d, vector<int> &start) {
-        long long prev = start[0];
-
-        for (int i = 1; i < start.size(); i++) {
-            long long next;
-
-            // Case 1: placing at least 'mid' away from previous
-            if (prev + mid <= start[i]) {
-                // can't start before its allowed window, so move to start[i]
-                next = start[i];
-            } 
-            else {
-                // can place at exactly prev + mid
-                next = prev + mid;
-            }
-
-            // If placement exceeds window limit, it's invalid
-            if (next > start[i] + d) {
+    bool canPlace(int mid,int d ,vector<int> & start){
+        long long  prev=start[0];
+        for(int i=1 ; i<start.size(); i++){
+            long long int next=max(prev+mid, static_cast<long long>(start[i]));
+            if (next > start[i] + d) { 
                 return false;
             }
-
-            prev = next;
+            prev=next;
         }
-
         return true;
     }
-
-    int maxPossibleScore(vector<int> &start, int d) {
+    int maxPossibleScore(vector<int> &start, int d)
+    {
         long long ans = 0;
         sort(start.begin(), start.end());
 
         long long low = 0;
         long long high = start.back() + d - start[0];
-
-        while (low <= high) {
+        while (low <= high)
+        {
             int mid = low - (low - high) / 2;
-
-            if (canPlace(mid, d, start)) {
+            if (canPlace(mid, d, start))
+            {
                 ans = mid;
                 low = mid + 1;
-            } else {
+            }
+            else
+            {
                 high = mid - 1;
             }
         }
-
         return ans;
     }
 };
