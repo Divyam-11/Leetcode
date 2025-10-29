b0d0b99815df2e613e5c03dff4bebe0d6f75808e
Neo <83936994+Divyam-11@users.noreply.github.com>
2025-10-29 22:50:44 +0530

    Time: 213 ms (35.59%) | Memory: 288.7 MB (49.12%) - LeetSync

---
+++ 3541-report-spam-message/report-spam-message.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    bool reportSpam(vector<string> &message, vector<string> &bannedWords)
+    {
+        unordered_set<string> st;
+        for (auto i : bannedWords)
+        {
+            st.insert(i);
+        }
+        int count = 0;
+        for (auto i : message)
+        {
+            if (st.find(i) != st.end())
+            {
+                count++;
+            }
+        }
+        return count > 1;
+    }
+};
