e33a75a01d7a4f697e10921f6501b64721613692
Neo <83936994+Divyam-11@users.noreply.github.com>
2025-10-29 22:42:02 +0530

    Time: 97 ms (54.96%) | Memory: 198 MB (46.69%) - LeetSync

---
+++ 3518-maximum-multiplication-score/maximum-multiplication-score.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    // a -> i
+    // b -> j
+    long long solve(int i, int j, vector<int> &a, vector<int> &b, vector<vector<long long>> &dp)
+    {
+
+        if (i < 0 && j >= 0)
+            return 0;
+        if (i >= 0 && j < 0)
+            return LLONG_MIN;
+        if (i == 0 && j == 0)
+            return a[0] * 1LL * b[0];
+        if (i < 0 || j < 0)
+            return LLONG_MIN;
+        if (dp[i][j] != LLONG_MIN)
+            return dp[i][j];
+        long long pick = LLONG_MIN;
+        long long temp = solve(i - 1, j - 1, a, b, dp);
+        if (temp != LLONG_MIN)
+            pick = a[i] * 1LL * b[j] + temp;
+        long long not_pick = solve(i, j - 1, a, b, dp);
+        return dp[i][j] = max(pick, not_pick);
+    }
+    long long maxScore(vector<int> &a, vector<int> &b)
+    {
+        vector<vector<long long>> dp(a.size(), vector<long long>(b.size(), LLONG_MIN));
+        return solve(a.size() - 1, b.size() - 1, a, b, dp);
+    }
+};
