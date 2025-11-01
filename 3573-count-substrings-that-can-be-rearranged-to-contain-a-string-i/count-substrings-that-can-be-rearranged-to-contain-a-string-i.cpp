5ec9e4bdfe9db63237b7af54ac5da74d94b69bc0
Neo <83936994+Divyam-11@users.noreply.github.com>
2025-11-01 14:23:28 +0530

    Time: 1229 ms (5.52%) | Memory: 15.5 MB (60.77%) - LeetSync

---
+++ 3573-count-substrings-that-can-be-rearranged-to-contain-a-string-i/count-substrings-that-can-be-rearranged-to-contain-a-string-i.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+public:
+    bool isValid(vector<int> &freq, vector<int> &word2_freq)
+    {
+        for (int i = 0; i < 26; i++)
+        {
+            if (freq[i] < word2_freq[i])
+                return false;
+        }
+        cout<<"hi"<<endl;
+        return true;
+    }
+    long long validSubstringCount(string word1, string word2)
+    {
+        vector<int> freq(26);
+        vector<int> word2_freq(26);
+        long long result = 0;
+        for (int i = 0; i < word2.size(); i++)
+        {
+            word2_freq[word2[i] - 'a']++;
+        }
+        int l = 0;
+        for (int r = 0; r < word1.size(); r++)
+        {
+            freq[word1[r] - 'a']++;
+            while (l <= r && isValid(freq, word2_freq))
+            {
+                result += word1.size() - r;
+                freq[word1[l]-'a']--;
+                l++;
+            }
+
+            
+        }
+        return result;
+    }
+};
