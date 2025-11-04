46a80c61865dcda88b6bcee893b922171a751ecd
Neo <83936994+Divyam-11@users.noreply.github.com>
2025-11-04 12:51:43 +0530

    Time: 408 ms (5.11%) | Memory: 37.2 MB (18.85%) - LeetSync

---
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+public:
+    int XSum(vector<int> &nums, int j, int k, int x)
+    {
+        unordered_map<int, int> mp;
+        for (int i = j; i < j + k; i++)
+        {
+            mp[nums[i]]++;
+        }
+        priority_queue<pair<int, int>, vector<pair<int, int>>> pq; // {freq,num}
+        for (auto i : mp)
+        {
+            cout<<i.first<<" "<<i.second<<endl;
+            pq.push({i.second, i.first});
+        }
+        int res = 0;
+        while (x-- && !pq.empty())
+        {
+            res += pq.top().second * pq.top().first;
+            cout<<pq.top().second<<"*"<<pq.top().first<<endl;
+            pq.pop();
+            
+        }
+        cout<<"----------------";
+        return res;
+    }
+    vector<int> findXSum(vector<int> &nums, int k, int x)
+    {
+        vector<int> res;
+        for (int i = 0; i < nums.size() - k + 1; i++)
+        {   
+            res.push_back(XSum(nums, i, k, x));
+            
+        }
+        return res;
+    }
+};
