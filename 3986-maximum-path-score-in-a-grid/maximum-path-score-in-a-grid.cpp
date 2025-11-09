fa22251855bc28701da293e3ddeb7b46ee9dae41
Neo <83936994+Divyam-11@users.noreply.github.com>
2025-11-09 18:34:44 +0530

    Time: 730 ms (33.33%) | Memory: 499.3 MB (0.00%) - LeetSync

--- 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
+++ 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
@@ -1,45 +1,42 @@
 class Solution
 {
 public:
-    int solve(int i, int j, int k, vector<vector<int>> &grid, vector<vector<vector<int>>> &dp)
-    {   if(k < 0 ) return -1;
-        if (i == 0 && j == 0)
-        {
-            return 0;
-        }
-        if (i < 0 || j < 0)
-        {
-            return -1;
-        }
-        if (dp[i][j][k] != INT_MIN)
-            return dp[i][j][k];
-        int cost = grid[i][j] > 0 ? 1 : 0;
-        if (cost <= k)
-        {
-            int up = solve(i - 1, j, k-cost, grid, dp);
-            int left = solve(i, j - 1, k-cost, grid, dp);
-            if (up == -1 && left == -1)
-            {
-                return dp[i][j][k] = -1;
-            }
-            if (up == -1)
-            {
-                up = INT_MIN;
-            }
-            if (left == -1)
-            {
-                left = INT_MIN;
-            }
-            return dp[i][j][k] = grid[i][j] + max(up, left);
-        }
-        return dp[i][j][k] = -1;
-    }
     int maxPathScore(vector<vector<int>> &grid, int k)
     {
-        vector<vector<vector<int>>> dp(grid.size(), vector<vector<int>>(grid[0].size(), vector<int>(k + 1, INT_MIN)));
-        int ans = solve(grid.size() - 1, grid[0].size() - 1, k, grid, dp);
-        if (ans < 0)
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(k + 1, -1)));
+        int start_cost = grid[0][0] > 0 ? 1 : 0;
+        if (start_cost <= k)
+            dp[0][0][k - start_cost] = grid[0][0];
+        else
             return -1;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                for (int rem = k; rem >= 0; rem--)
+                {
+                    if (dp[i][j][rem] == -1)
+                        continue;
+
+                    int cost_down = 0, cost_right = 0;
+                    if (i + 1 < n)
+                        cost_down = grid[i + 1][j] > 0 ? 1 : 0;
+                    if (j + 1 < m)
+                        cost_right = grid[i][j + 1] > 0 ? 1 : 0;
+
+                    if (i + 1 < n && rem >= cost_down)
+                        dp[i + 1][j][rem - cost_down] = max(dp[i + 1][j][rem - cost_down], dp[i][j][rem] + grid[i + 1][j]);
+                    if (j + 1 < m && rem >= cost_right)
+                        dp[i][j + 1][rem - cost_right] = max(dp[i][j + 1][rem - cost_right], dp[i][j][rem] + grid[i][j + 1]);
+                }
+            }
+        }
+
+        int ans = -1;
+        for (int rem = 0; rem <= k; rem++)
+            ans = max(ans, dp[n - 1][m - 1][rem]);
         return ans;
     }
 };
