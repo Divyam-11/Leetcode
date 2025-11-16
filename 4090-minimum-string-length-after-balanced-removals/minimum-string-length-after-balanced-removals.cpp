05faa21f086aad412dc86d50c5af3e4999b5ef39
Neo <83936994+Divyam-11@users.noreply.github.com>
2025-11-16 10:53:45 +0530

    Time: 2 ms (66.67%) | Memory: 15.3 MB (8.33%) - LeetSync

---
+++ 4090-minimum-string-length-after-balanced-removals/minimum-string-length-after-balanced-removals.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    int minLengthAfterRemovals(string s)
+    {
+        stack<int> st;
+        int count = 0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == 'a')
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+        }
+        return abs(count);
+    }
+};
