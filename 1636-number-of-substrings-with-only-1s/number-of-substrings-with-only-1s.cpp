d7fb2d120824db1a4b3fbcbd8f72c97694c564cc
Neo <83936994+Divyam-11@users.noreply.github.com>
2025-11-16 10:02:05 +0530

    Time: 1 ms (45.50%) | Memory: 11.4 MB (99.23%) - LeetSync

---
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    const int MOD = 1e9 + 7;
+    int numSub(string s)
+    {
+        long long res = 0;
+        int len = 0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == '1')
+            {
+                len++;
+            }
+            if (s[i] == '0')
+            {
+                res += (len *1LL* (len + 1)) / 2;
+                res %= MOD;
+                len = 0;
+            }
+        }
+            res += (len * (len + 1)) / 2;
+                res %= MOD;
+                len = 0;
+        return res;
+    }
+};
