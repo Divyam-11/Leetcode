1e562b04ac74dd8ec0fb1b11f19e875efeca6bea
Neo <83936994+Divyam-11@users.noreply.github.com>
2025-12-26 11:34:44 +0530

    Time: 219 ms (5.03%) | Memory: 17.5 MB (56.07%) - LeetSync

---
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+public:
+    int bestClosingTime(string customers)
+    {
+        int time = INT_MIN;
+        int res = customers.size();
+        int sum = 0;
+        vector<int> prefix(customers.size());
+        for (int i = customers.size()-1; i >= 0; i--)
+        {
+
+            if (customers[i] == 'Y')
+            {
+                sum++;
+            }
+            prefix[i] = sum;
+        }
+        sum = 0;
+        for (int i = 0; i < customers.size(); i++)
+        {
+            if (prefix[i] + sum < res)
+            {
+                cout<<res<<endl;
+                res = prefix[i] + sum;
+                time = i;
+            }
+
+            if (customers[i] == 'N')
+                sum++;
+        }
+        if(sum < res){
+            time = customers.size();
+
+        }
+        return time;
+    }
+};
