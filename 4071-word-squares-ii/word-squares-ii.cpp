e3dfe9d6f244703a30282ff65d60317b0d403d95
Neo <83936994+Divyam-11@users.noreply.github.com>
2026-01-04 13:00:22 +0530

    Time: 303 ms (9.32%) | Memory: 60.1 MB (44.13%) - LeetSync

---
+++ 4071-word-squares-ii/word-squares-ii.cpp
@@ -0,0 +1,42 @@
+class Solution
+{
+public:
+    bool isValid(string top, string left, string right, string bottom)
+    {
+        if (top[0] == left[0] && top[3] == right[0] && bottom[0] == left[3] && bottom[3] == right[3])
+            return true;
+        return false;
+    }
+    vector<vector<string>> wordSquares(vector<string> &words)
+    {
+        vector<vector<string>> result;
+        for (int i = 0; i < words.size(); i++)
+        {
+            for (int j = 0; j < words.size(); j++)
+            {
+                if (j == i)
+                    continue;
+                for (int k = 0; k < words.size(); k++)
+                {
+                    if (k == j || k == i)
+                        continue;
+                    for (int l = 0; l < words.size(); l++)
+                    {
+                        if (l == i || l == j || l == k)
+                            continue;
+                        string top = words[i];
+                        string left = words[j];
+                        string right = words[k];
+                        string bottom = words[l];
+                        if (isValid(top, left, right, bottom))
+                        {
+                            result.push_back({top, left, right, bottom});
+                        }
+                    }
+                }
+            }
+        }
+        sort(result.begin(),result.end());
+               return result;
+    }
+};
