7ef5c2727c9109499f7c0f0d3ae8d9ebe12e3050
Neo <83936994+Divyam-11@users.noreply.github.com>
2026-01-23 10:16:38 +0530

    Time: 15 ms (18.73%) | Memory: 17.3 MB (29.52%) - LeetSync

--- 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.cpp
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.cpp
@@ -10,7 +10,7 @@ public:
         {
             if (s[i] == '(')
             {   
-                cout<<"hey"<<endl;
+               
                 st.push('(');
                 
             }
