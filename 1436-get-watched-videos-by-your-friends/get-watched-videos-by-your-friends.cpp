d79c086f086518db13c0fe8bc26de1ab350def11
Neo <83936994+Divyam-11@users.noreply.github.com>
2026-01-24 12:24:57 +0530

    Time: 40 ms (37.03%) | Memory: 45.5 MB (41.93%) - LeetSync

---
+++ 1436-get-watched-videos-by-your-friends/get-watched-videos-by-your-friends.cpp
@@ -0,0 +1,50 @@
+class Solution
+{
+public:
+    vector<string> watchedVideosByFriends(vector<vector<string>> &watchedVideos, vector<vector<int>> &friends, int id, int level)
+    {
+        int n = friends.size();
+        queue<pair<int, int>> q; // {id,level};
+        q.push({id, 0});
+        unordered_map<string, int> mp; // {video,freq}
+        vector<int> visited(n);
+        visited[id] =1;
+        while (!q.empty())
+        {
+            int temp_id = q.front().first;
+            int lvl = q.front().second;
+
+            q.pop();
+            if (lvl == level)
+            {
+                for (auto vid : watchedVideos[temp_id])
+                {
+                    mp[vid]++;
+                }
+            }
+            if (lvl < level)
+            {
+                for (auto frnd : friends[temp_id])
+                {
+                    if (!visited[frnd])
+                    {
+                        q.push({frnd, lvl + 1});
+                        visited[frnd] = 1;
+                    }
+                }
+            }
+        }
+        vector<pair<int, string>> temp;
+        for (auto i : mp)
+        {
+            temp.push_back({i.second, i.first});
+        }
+        sort(temp.begin(), temp.end());
+        vector<string> res;
+        for (int i = 0; i < temp.size(); i++)
+        {
+            res.push_back(temp[i].second);
+        }
+        return res;
+    }
+};
