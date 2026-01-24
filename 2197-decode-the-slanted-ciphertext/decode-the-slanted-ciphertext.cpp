57ca7c297bf89fed5eda6d0cbf99364ceb44566f
Neo <83936994+Divyam-11@users.noreply.github.com>
2026-01-24 11:54:42 +0530

    Time: 33 ms (57.76%) | Memory: 41.8 MB (72.41%) - LeetSync

--- 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.cpp
+++ 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.cpp
@@ -4,33 +4,21 @@ public:
     string decodeCiphertext(string encodedText, int rows)
     {   if(encodedText.empty()) return "";
         int col = encodedText.size() / rows;
-        vector<string> grid(rows, string(col, ' '));
-        // cout << rows << " " << col << endl;
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < col; j++)
-            {
-                grid[i][j] = encodedText[col * i + j];
-                // cout << grid[i][j];
-            }
-            // cout << endl;
-        }
         string res;
         for (int i = 0; i < col; i++)
         {
             int x = 0;
-            int y = i;
-            while (x < rows && y < col)
+            int j = i;
+            while (x < rows && j < col)
             {
-                res.push_back(grid[x][y]);
+
+                res.push_back(encodedText[x * col + j]);
                 x++;
-                y++;
+                j++;
             }
         }
         while (res.back() == ' ')
-        {
             res.pop_back();
-        }
         return res;
     }
 };
