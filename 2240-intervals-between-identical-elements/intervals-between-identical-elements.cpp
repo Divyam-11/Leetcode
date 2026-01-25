d4d391d0bdaee4839fc0d13606ec19932c5080a3
Neo <83936994+Divyam-11@users.noreply.github.com>
2026-01-25 12:46:42 +0530

    Time: 103 ms (73.13%) | Memory: 123.7 MB (90.55%) - LeetSync

--- 2240-intervals-between-identical-elements/intervals-between-identical-elements.cpp
+++ 2240-intervals-between-identical-elements/intervals-between-identical-elements.cpp
@@ -4,7 +4,8 @@ public:
     vector<long long> getDistances(vector<int> &arr)
     {
         vector<long long> ans(arr.size());
-        unordered_map<int, vector<long long>> mp;
+        unordered_map<int, tuple<long long, long long, long long>> mp;
+
         for (int i = 0; i < arr.size(); i++)
         {
             if (mp.find(arr[i]) == mp.end())
@@ -14,11 +15,17 @@ public:
             else
             {
                 auto temp = mp[arr[i]];
-                ans[i] += temp[2] + temp[0] * abs(i - temp[1]);
-                mp[arr[i]] = {temp[0] + 1, i, ans[i]};
+                long long cnt = get<0>(temp);
+                long long last = get<1>(temp);
+                long long sum = get<2>(temp);
+
+                ans[i] += sum + cnt * abs(i - last);
+                mp[arr[i]] = {cnt + 1, i, ans[i]};
             }
         }
+
         mp.clear();
+
         for (int i = arr.size() - 1; i >= 0; i--)
         {
             if (mp.find(arr[i]) == mp.end())
@@ -28,9 +35,13 @@ public:
             else
             {
                 auto temp = mp[arr[i]];
-                long long sc = temp[2] + temp[0] * abs(i - temp[1]);
-                ans[i] +=  sc;
-                mp[arr[i]] = {temp[0] + 1, i, sc};
+                long long cnt = get<0>(temp);
+                long long last = get<1>(temp);
+                long long sum = get<2>(temp);
+
+                long long sc = sum + cnt * abs(i - last);
+                ans[i] += sc;
+                mp[arr[i]] = {cnt + 1, i, sc};
             }
         }
         return ans;
