cd06502d7e11958f7b38a53d322b1a2d09432640
Neo <83936994+Divyam-11@users.noreply.github.com>
2026-01-26 13:30:21 +0530

    Time: 59 ms (66.41%) | Memory: 80.2 MB (68.75%) - LeetSync

--- 2548-destroy-sequential-targets/destroy-sequential-targets.cpp
+++ 2548-destroy-sequential-targets/destroy-sequential-targets.cpp
@@ -3,7 +3,7 @@ class Solution
 public:
     int destroyTargets(vector<int> &nums, int space)
     {
-        sort(nums.begin(), nums.end());
+        
         unordered_map<int, int> mp;
         int maxfreq = 0;
         int maxEle = 0;
@@ -18,13 +18,13 @@ public:
                 maxEle = ele;
             }
         }
-        int res = 0;
+        int res = INT_MAX;
         for(int i = 0 ;i < nums.size();i++){
 
             if(mp[nums[i] % space ]== maxfreq){
-                return nums[i];
+                res = min(nums[i],res);
             }
         }
-        return maxEle;
+        return res;
     }
 };
