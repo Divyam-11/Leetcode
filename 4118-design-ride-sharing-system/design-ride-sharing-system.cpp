74f38ba90d3ae089b5dc312c24cad5a583808e58
Neo <83936994+Divyam-11@users.noreply.github.com>
2026-02-01 13:13:00 +0530

    Time: 76 ms (13.03%) | Memory: 153.2 MB (19.98%) - LeetSync

--- 4118-design-ride-sharing-system/design-ride-sharing-system.cpp
+++ 4118-design-ride-sharing-system/design-ride-sharing-system.cpp
@@ -6,7 +6,7 @@ public:
     set<pair<int, int>> riders;
     set<pair<int, int>> drivers;
     unordered_map<int, int> id_idx1;
-    // unordered_map<int, int> id_idx2;
+    unordered_map<int, int> id_idx2;
     RideSharingSystem()
     {
         n1 = 0;
@@ -23,20 +23,20 @@ public:
     void addDriver(int driverId)
     {
         drivers.insert({n2, driverId});
-        // id_idx2[driverId] = n2;
+        id_idx2[driverId] = n2;
         n2++;
     }
 
     vector<int> matchDriverWithRider()
     {
         if (riders.empty() || drivers.empty())
-            return {-1,-1};
+            return {-1, -1};
         auto temp1 = *riders.begin();
         auto temp2 = *drivers.begin();
         riders.erase(riders.begin());
         drivers.erase(drivers.begin());
         id_idx1.erase(temp1.second);
-        // id_idx2.erase(temp2.second);
+        id_idx2.erase(temp2.second);
         return {temp2.second, temp1.second};
     }
 
