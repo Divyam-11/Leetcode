302f4c8dbd5bc4f53cad389107f8ae3c5595bb85
Neo <83936994+Divyam-11@users.noreply.github.com>
2026-02-01 13:10:38 +0530

    Time: 46 ms (59.82%) | Memory: 153.3 MB (19.98%) - LeetSync

---
+++ 4118-design-ride-sharing-system/design-ride-sharing-system.cpp
@@ -0,0 +1,51 @@
+class RideSharingSystem
+{
+public:
+    int n1;
+    int n2;
+    set<pair<int, int>> riders;
+    set<pair<int, int>> drivers;
+    unordered_map<int, int> id_idx1;
+    unordered_map<int, int> id_idx2;
+    RideSharingSystem()
+    {
+        n1 = 0;
+        n2 = 0;
+    }
+
+    void addRider(int riderId)
+    {
+        riders.insert({n1, riderId});
+        id_idx1[riderId] = n1;
+        n1++;
+    }
+
+    void addDriver(int driverId)
+    {
+        drivers.insert({n2, driverId});
+        id_idx2[driverId] = n2;
+        n2++;
+    }
+
+    vector<int> matchDriverWithRider()
+    {
+        if (riders.empty() || drivers.empty())
+            return {-1, -1};
+        auto temp1 = *riders.begin();
+        auto temp2 = *drivers.begin();
+        riders.erase(riders.begin());
+        drivers.erase(drivers.begin());
+        id_idx1.erase(temp1.second);
+        id_idx2.erase(temp2.second);
+        return {temp2.second, temp1.second};
+    }
+
+    void cancelRider(int riderId)
+    {
+        if (id_idx1.find(riderId) != id_idx1.end())
+        {
+            riders.erase({id_idx1[riderId], riderId});
+            id_idx1.erase(riderId);
+        }
+    }
+};
