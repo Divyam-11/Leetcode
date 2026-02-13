865bf84d00b8221e708cbcd4d13e2c1f6565cd64
Neo <83936994+Divyam-11@users.noreply.github.com>
2026-02-13 18:22:06 +0530

    Time: 1822 ms (5.47%) | Memory: 553.4 MB (52.02%) - LeetSync

--- 4202-maximum-score-using-exactly-k-pairs/maximum-score-using-exactly-k-pairs.cpp
+++ 4202-maximum-score-using-exactly-k-pairs/maximum-score-using-exactly-k-pairs.cpp
@@ -1,23 +1,16 @@
-class Solution
-{
+class Solution {
 public:
-    long long solve(int i, int j, int k, vector<int> &nums1, vector<int> &nums2, vector<vector<vector<long long>>> &dp)
-    {
-        if (k == 0)
-            return 0;
-        if (i >= nums1.size() || j >= nums2.size())
-            return -1e15;
-        if (dp[i][j][k] != LLONG_MIN)
-            return dp[i][j][k];
-        long long pick = nums1[i] *1LL *nums2[j] + solve(i + 1, j + 1, k - 1, nums1, nums2, dp);
-        long long pick2 = solve(i + 1, j, k, nums1, nums2, dp);
-        long long pick3 = solve(i, j + 1, k, nums1, nums2, dp);
-        return dp[i][j][k] = max({pick, pick2, pick3});
+    long long solve(int idx1, int idx2, vector<int>& nums1, vector<int>& nums2,int k, vector<vector<vector<long long>>>& dp) {
+        if (k == 0) return 0;
+        if (idx1 >= nums1.size() || idx2 >= nums2.size()) return -1e15;
+        if (dp[idx1][idx2][k] != -2e18) return dp[idx1][idx2][k];
+        long long take = 1LL * nums1[idx1] * nums2[idx2] + solve(idx1 + 1, idx2 + 1, nums1, nums2, k - 1, dp);
+        long long skip1 = solve(idx1 + 1, idx2, nums1, nums2, k, dp);
+        long long skip2 = solve(idx1, idx2 + 1, nums1, nums2, k, dp);
+        return dp[idx1][idx2][k] = max({take, skip1, skip2});
     }
-    long long maxScore(vector<int> &nums1, vector<int> &nums2, int k)
-
-    {
-        vector<vector<vector<long long>>> dp(nums1.size(), vector<vector<long long>>(nums2.size(), vector<long long>(k + 1, LLONG_MIN)));
-        return solve(0, 0, k, nums1, nums2, dp);
+    long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
+        vector<vector<vector<long long>>> dp(nums1.size(), vector<vector<long long>>(nums2.size(), vector<long long>(k+1, -2e18)));
+        return solve(0, 0, nums1, nums2, k, dp);
     }
 };
