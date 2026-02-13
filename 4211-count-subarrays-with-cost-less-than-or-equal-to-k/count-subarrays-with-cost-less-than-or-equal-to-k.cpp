47850e14cfb72b7f8a110ec5d7dc5595c07b978c
Neo <83936994+Divyam-11@users.noreply.github.com>
2026-02-13 20:32:09 +0530

    Time: 582 ms (8.30%) | Memory: 299.2 MB (31.78%) - LeetSync

---
+++ 4211-count-subarrays-with-cost-less-than-or-equal-to-k/count-subarrays-with-cost-less-than-or-equal-to-k.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+public:
+    long long countSubarrays(vector<int> &nums, long long k)
+    {
+        long long count = 0;
+        set<pair<long long, int>> st; // {val,idx}
+        int l = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            st.insert({nums[i], i});
+            long long mi = (*st.begin()).first;
+            long long mx = (*st.rbegin()).first;
+            while (!st.empty() && (mx - mi) * (i - l + 1) > k)
+            {
+                
+                st.erase({nums[l], l});
+                l++;
+                mi = (*st.begin()).first;
+                mx = (*st.rbegin()).first;
+            }
+            count += (i - l + 1);
+        }
+        return count;
+    }
+};
