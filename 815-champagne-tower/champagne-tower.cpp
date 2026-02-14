1c996d482d43ad712babeb2dc769127076367f48
Neo <83936994+Divyam-11@users.noreply.github.com>
2026-02-14 11:09:13 +0530

    Time: 11 ms (25.42%) | Memory: 18.9 MB (31.48%) - LeetSync

--- 815-champagne-tower/champagne-tower.cpp
+++ 815-champagne-tower/champagne-tower.cpp
@@ -1,24 +1,27 @@
-class Solution
-{
+class Solution {
 public:
-    double champagneTower(int poured, int query_row, int query_glass)
+    double champagneTower(int poured, int query_row, int query_glass) 
     {
-        vector<vector<double>> grid(101, vector<double>(101, 0));
-        grid[0][0] = poured;
-        for (int i = 0; i < 100; i++)
+        vector<double> row(101, 0.0);
+        row[0] = poured;
+        
+        for (int i = 0; i < query_row; i++) 
         {
-            for (int j = 0; j <= i; j++)
+            vector<double> next(101, 0.0);
+            
+            for (int j = 0; j <= i; j++) 
             {
-                if (grid[i][j] > 1.0)
+                if (row[j] > 1.0) 
                 {
-                    double excess = grid[i][j] - 1.0;
-                    grid[i][j] = 1.0;
-                    grid[i + 1][j] += excess / 2;
-                    grid[i + 1][j + 1] += excess / 2;
-
+                    double excess = row[j] - 1.0;
+                    next[j] += excess / 2.0;
+                    next[j + 1] += excess / 2.0;
                 }
             }
+            
+            row = next;
         }
-        return grid[query_row][query_glass];
+        
+        return min(1.0, row[query_glass]);
     }
 };
