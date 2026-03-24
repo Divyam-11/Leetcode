5b577cabd2039a666187877f7f092f209612a73b
Neo <83936994+Divyam-11@users.noreply.github.com>
2026-03-24 19:43:14 +0530

    Time: 79 ms (24.35%) | Memory: 148.7 MB (28.41%) - LeetSync

--- 3031-construct-product-matrix/construct-product-matrix.cpp
+++ 3031-construct-product-matrix/construct-product-matrix.cpp
@@ -5,31 +5,32 @@ public:
     {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> prefix(grid.size(), vector<int>(grid[0].size()));
+        // vector<vector<int>> prefix(grid.size(), vector<int>(grid[0].size()));
         vector<vector<int>> suffix(grid.size(), vector<int>(grid[0].size()));
         vector<vector<int>> p(grid.size(), vector<int>(grid[0].size()));
         long long pre = 1;
         long long post = 1;
-        
+
         for (int i = 0; i < grid.size(); i++)
         {
             for (int j = 0; j < grid[0].size(); j++)
             {
                 int x = m - i - 1;
                 int y = n - j - 1;
-                prefix[i][j] = pre;
+                // prefix[i][j] = pre;
                 suffix[x][y] = post;
-                pre = (pre * grid[i][j]) % 12345;
+                // pre = (pre * grid[i][j]) % 12345;
                 post = (post * grid[x][y]) % 12345;
             }
         }
-        
+
         for (int i = 0; i < grid.size(); i++)
         {
             for (int j = 0; j < grid[0].size(); j++)
             {
-                p[i][j] = (prefix[i][j] * suffix[i][j]) % 12345;
-                // cout<<suffix[i][j]<<endl;
+                p[i][j] = (pre * suffix[i][j]) % 12345;
+                pre = (pre * grid[i][j]) % 12345;
+                // cout << suffix[i][j] << endl;
             }
         }
         return p;
