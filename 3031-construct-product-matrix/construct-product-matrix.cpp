c1e7f3bd496a2b40b1671c99c09c38fcf63988d2
Neo <83936994+Divyam-11@users.noreply.github.com>
2026-03-24 19:43:48 +0530

    Time: 27 ms (87.45%) | Memory: 132 MB (80.07%) - LeetSync

--- 3031-construct-product-matrix/construct-product-matrix.cpp
+++ 3031-construct-product-matrix/construct-product-matrix.cpp
@@ -7,7 +7,7 @@ public:
         int n = grid[0].size();
         // vector<vector<int>> prefix(grid.size(), vector<int>(grid[0].size()));
         vector<vector<int>> suffix(grid.size(), vector<int>(grid[0].size()));
-        vector<vector<int>> p(grid.size(), vector<int>(grid[0].size()));
+        // vector<vector<int>> p(grid.size(), vector<int>(grid[0].size()));
         long long pre = 1;
         long long post = 1;
 
@@ -28,11 +28,11 @@ public:
         {
             for (int j = 0; j < grid[0].size(); j++)
             {
-                p[i][j] = (pre * suffix[i][j]) % 12345;
+                suffix[i][j] = (pre * suffix[i][j]) % 12345;
                 pre = (pre * grid[i][j]) % 12345;
                 // cout << suffix[i][j] << endl;
             }
         }
-        return p;
+        return suffix;
     }
 };
