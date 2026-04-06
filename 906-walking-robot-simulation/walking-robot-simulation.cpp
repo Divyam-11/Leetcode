b8a7a49967745e5132e178a7c773b7e115a4c769
Neo <83936994+Divyam-11@users.noreply.github.com>
2026-04-06 06:17:03 +0530

    Time: 23 ms (66.40%) | Memory: 37.6 MB (75.47%) - LeetSync

---
+++ 906-walking-robot-simulation/walking-robot-simulation.cpp
@@ -0,0 +1,55 @@
+class Solution
+{
+public:
+    int robotSim(vector<int> &commands, vector<vector<int>> &obstacles)
+    {
+        int res = 0;
+        int a = 0;
+        int b = 0;
+        vector<pair<int, int>> dir = {{1, 0}, {0, -1}, {-1, 0}, {0, 1}};
+        int ptr = 3;
+        set<pair<int, int>> st;
+        for (int i = 0; i < obstacles.size(); i++)
+        {
+            st.insert({obstacles[i][0], obstacles[i][1]});
+        }
+        for (int i = 0; i < commands.size(); i++)
+        {
+            if (commands[i] < 0)
+            {
+                if (commands[i] == -2)
+                {
+                    ptr = ((ptr - 1) + 4) % 4;
+                }
+                if (commands[i] == -1)
+                {
+                    ptr = (ptr + 1) % 4;
+                }
+            }
+            else
+            {
+                int x = dir[ptr].first;
+                int y = dir[ptr].second;
+                int k = commands[i];
+                while (k)
+                {
+                    int temp_i = a + x * 1;
+                    int temp_j = b + y * 1;
+                    if (st.find({temp_i, temp_j}) != st.end())
+                    {
+                        break;
+                    }
+                    a = temp_i;
+                    b = temp_j;
+                    
+                    
+                    res = max(res, a * a + b * b);
+                    k--;
+                }
+                res = max(res, a * a + b * b);
+            
+            }
+        }
+        return res;
+    }
+};
