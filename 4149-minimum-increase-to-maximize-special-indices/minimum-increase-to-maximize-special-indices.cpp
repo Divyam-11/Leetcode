ef18299dd4788e5d5b9905448a84650953e0a605
Neo <83936994+Divyam-11@users.noreply.github.com>
2026-04-10 10:35:26 +0530

    Time: 27 ms (39.71%) | Memory: 158.3 MB (58.44%) - LeetSync

--- 4149-minimum-increase-to-maximize-special-indices/minimum-increase-to-maximize-special-indices.cpp
+++ 4149-minimum-increase-to-maximize-special-indices/minimum-increase-to-maximize-special-indices.cpp
@@ -4,11 +4,9 @@ public:
     long long minIncrease(vector<int> &nums)
     {
         long long ops = 0;
-        int n = nums.size();
-
-        if (n % 2 == 1)
+        if (nums.size() % 2 == 1)
         {
-            for (int i = 1; i < n - 1; i += 2)
+            for (int i = 1; i < nums.size() - 1; i += 2)
             {
                 long long temp = max(nums[i + 1], nums[i - 1]) - nums[i] + 1;
                 if (temp > 0)
@@ -16,57 +14,55 @@ public:
             }
             return ops;
         }
-
         long long res = LLONG_MAX;
-
-        for (int i = 1; i < n - 1; i += 2)
+        // cout << "weee" << endl;
+        for (int i = 1; i < nums.size() - 1; i += 2)
         {
             long long temp = max(nums[i + 1], nums[i - 1]) - nums[i] + 1;
             if (temp > 0)
                 ops += temp;
         }
         res = min(ops, res);
-
         ops = 0;
-
-        for (int i = 2; i < n - 1; i += 2)
+        // cout << "weee" << endl;
+        for (int i = 2; i < nums.size() - 1; i += 2)
         {
             long long temp = max(nums[i + 1], nums[i - 1]) - nums[i] + 1;
             if (temp > 0)
                 ops += temp;
         }
         res = min(ops, res);
-
-        vector<long long> prefix(n, 0);
-        vector<long long> suffix(n, 0);
-
+        vector<long long> prefix(nums.size());
+        vector<long long> suffix(nums.size());
+        int sum = 0;
+        // cout << "weee" << endl;
         prefix[1] = max(0LL, (long long)max(nums[2], nums[0]) - nums[1] + 1);
-        for (int i = 2; i < n - 1; i++)
+        for (int i = 2; i < nums.size() - 1; i++)
         {
             long long temp = max(nums[i + 1], nums[i - 1]) - nums[i] + 1;
             temp = max(temp, 0LL);
             prefix[i] = prefix[i - 2] + temp;
         }
-
-        int x = n - 2;
-        suffix[x] = max(0LL, (long long)max(nums[x + 1], nums[x - 1]) - nums[x] + 1);
-
+        // cout << "weee" << endl;
+        int x = nums.size() - 2;
+        suffix[x] = max(0, max(nums[x + 1], nums[x - 1]) - nums[x] + 1);
+        // cout << "weee" << endl;
         for (int i = x - 1; i >= 2; i--)
         {
             long long temp = max(nums[i + 1], nums[i - 1]) - nums[i] + 1;
             temp = max(temp, 0LL);
             suffix[i] = suffix[i + 2] + temp;
         }
-
-        for (int i = 1; i < n - 2; i += 2)
+        // cout << "weee" << endl;
+        for (int i = 1; i < nums.size()-2; i+=2)
         {
             long long pre = prefix[i];
             long long suf = 0;
-            if (i + 3 < n)
+            if (i + 3 < nums.size())
                 suf = suffix[i + 3];
             res = min(suf + pre, res);
         }
-
+        // cout << "weee" << endl;
         return res;
     }
 };
