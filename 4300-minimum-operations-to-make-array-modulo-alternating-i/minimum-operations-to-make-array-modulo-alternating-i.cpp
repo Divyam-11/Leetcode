ed6f79d31fcbab3e3a3be3201aa9464886d2d92a
Neo <83936994+Divyam-11@users.noreply.github.com>
2026-05-24 23:22:02 +0530

    Time: 23 ms (41.67%) | Memory: 27.4 MB (25.00%) - LeetSync

---
+++ 4300-minimum-operations-to-make-array-modulo-alternating-i/minimum-operations-to-make-array-modulo-alternating-i.cpp
@@ -0,0 +1,54 @@
+class Solution
+{
+public:
+    int minOperations(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> evenmp;
+        unordered_map<int, int> oddmp;
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            evenmp[nums[i]]++;
+            oddmp[nums[i + 1]]++;
+        }
+        priority_queue<pair<int, int>> oddpq;
+        priority_queue<pair<int, int>> evenpq;
+        for (int x = 0; x < k; x++)
+        {
+            int odd_dis = 0;
+            int eve_dis = 0;
+            for (int i = 0; i < nums.size(); i++)
+            {
+                int rem = nums[i] % k;
+                if (i % 2)
+                {
+                    odd_dis += min({abs(rem - x),
+                                    abs(rem + k - x),
+                                    abs(rem - k - x)});
+                }
+                else
+                {
+                    eve_dis += min({abs(rem - x),
+                                    abs(rem + k - x),
+                                    abs(rem - k - x)});
+                }
+            }
+            oddpq.push({-odd_dis, x});
+            evenpq.push({-eve_dis, x});
+        }
+        if (oddpq.top().second != evenpq.top().second)
+        {
+            return -1 * (oddpq.top().first + evenpq.top().first);
+        }
+        else
+        {
+            int top1o = oddpq.top().first * -1;
+            int top1e = evenpq.top().first * -1;
+            oddpq.pop();
+            evenpq.pop();
+            int top2o = oddpq.top().first * -1;
+            int top2e = evenpq.top().first * -1;
+            return min({top1o + top2e, top1e + top2o});
+        }
+        return 67;
+    }
+};
