fb390d9c1cceeab8cb3c0f6e0f6be79fbae285d1
Neo <83936994+Divyam-11@users.noreply.github.com>
2026-05-27 12:54:19 +0530

    Time: 7 ms (56.95%) | Memory: 97.5 MB (13.68%) - LeetSync

--- 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -3,29 +3,29 @@ class Solution
 public:
     bool solve(int idx, vector<int> &nums, vector<int> &dp)
     {
-        if (idx == nums.size())
+        if (idx == -1)
             return true;
-        if (idx > nums.size())
+        if (idx < 0)
             return false;
         if (dp[idx] != -1)
             return dp[idx];
         bool res = false;
-        if (idx + 1 < nums.size())
+        if (idx - 1 >= 0)
         {
-            if (nums[idx] == nums[idx + 1])
+            if (nums[idx] == nums[idx - 1])
             {
-                res = res || solve(idx + 2, nums, dp);
+                res = res || solve(idx - 2, nums, dp);
             }
         }
-        if (idx + 2 < nums.size())
+        if (idx - 2 >= 0)
         {
-            if (nums[idx] == nums[idx + 1] && nums[idx] == nums[idx + 2])
+            if (nums[idx] == nums[idx - 1] && nums[idx] == nums[idx - 2])
             {
-                res = res || solve(idx + 3, nums, dp);
+                res = res || solve(idx - 3, nums, dp);
             }
-            else if (nums[idx] + 1 == nums[idx + 1] && nums[idx] + 2 == nums[idx + 2])
+            else if (nums[idx] - 1 == nums[idx - 1] && nums[idx] - 2 == nums[idx - 2])
             {
-                res = res || solve(idx + 3, nums, dp);
+                res = res || solve(idx - 3, nums, dp);
             }
         }
         return dp[idx] = res;
@@ -33,6 +33,6 @@ public:
     bool validPartition(vector<int> &nums)
     {
         vector<int> dp(nums.size(), -1);
-        return solve(0, nums, dp);
+        return solve(nums.size()-1, nums, dp);
     }
 };
