24808e3d00f25e7e650285b1d068848a02ceb215
Neo <83936994+Divyam-11@users.noreply.github.com>
2026-06-21 13:01:12 +0530

    Time: 1526 ms (72.35%), Space: 472.9 MB (51.94%) - LeetHub

--- 3960-frequency-balance-subarray/3960-frequency-balance-subarray.cpp
+++ 3960-frequency-balance-subarray/3960-frequency-balance-subarray.cpp
@@ -12,10 +12,12 @@ public:
             {
                 int before = mp[nums[j]];
                 if (before != 0)
-                    fp[before]--;
-                if (fp[before] == 0)
                 {
-                    fp.erase(before);
+                    fp[before]--;
+                    if (fp[before] == 0)
+                    {
+                        fp.erase(before);
+                    }
                 }
 
                 mp[nums[j]]++;
@@ -23,13 +25,13 @@ public:
                 {
                     res = max(res, j - i + 1);
                 }
-                int after = mp[nums[j]];
-                fp[after]++;
+
+                fp[before + 1]++;
                 if (fp.size() == 2)
                 {
                     int val1 = (*fp.begin()).first;
                     int val3 = -1;
-                    if(val1 %2 == 0) val3 = val1/2;
+                    if(val1 % 2 == 0) val3 = val1/2;
                     if (fp.find(val1 * 2) != fp.end() || fp.find(val3) != fp.end())
                     {
                         res = max(res, j - i + 1);
