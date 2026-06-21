ee99e79dd96e8e0daa961db8f48f12cb8189dfb8
Neo <83936994+Divyam-11@users.noreply.github.com>
2026-06-21 17:10:54 +0530

    Time: 39 ms (64.96%), Space: 156.8 MB (66.76%) - LeetHub

---
+++ 3961-maximize-sum-of-device-ratings/3961-maximize-sum-of-device-ratings.cpp
@@ -0,0 +1,44 @@
+class Solution
+{
+public:
+    pair<int, int> returnSmallest(vector<int> &unit)
+    {
+        if (unit.size() == 1)
+            return {unit[0], unit[0]};
+        // int smallest = INT_MAX, second_smallest = INT_MAX;
+        // for (int i = 0; i < unit.size(); i++)
+        // {
+        //     if (unit[i] < smallest)
+        //     {
+        //         smallest = unit[i];
+        //         second_smallest = smallest;
+        //     }
+        //     else if (unit[i] <= smallest)
+        //     {
+        //         second_smallest = unit[i];
+        //     }
+        // }
+        // return {smallest, second_smallest};
+        sort(unit.begin(), unit.end());
+        return {unit[0], unit[1]};
+    }
+    long long maxRatings(vector<vector<int>> &units)
+    {
+        int m = units.size();
+        int n = units[0].size();
+
+        long long sum = 0;
+        int smallest = INT_MAX;
+        int smallest_second_smallest = INT_MAX;
+        for (int i = 0; i < units.size(); i++)
+        {
+            auto temp = returnSmallest(units[i]);
+            sum += temp.second;
+            smallest = min(smallest, temp.first);
+            smallest_second_smallest = min(smallest_second_smallest, temp.second);
+        }
+        sum -= smallest_second_smallest;
+        sum += smallest;
+        return sum;
+    }
+};
