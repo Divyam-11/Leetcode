24383afb0f6adc150edcf76cd5ab837a6df4d377
Neo <83936994+Divyam-11@users.noreply.github.com>
2026-06-21 12:56:02 +0530

    Time: 2611 ms (5.71%), Space: 518.7 MB (5.05%) - LeetHub

--- 3960-frequency-balance-subarray/3960-frequency-balance-subarray.cpp
+++ 3960-frequency-balance-subarray/3960-frequency-balance-subarray.cpp
@@ -1,28 +1,45 @@
-class Solution {
+class Solution
+{
 public:
-    int getLength(vector<int>& nums) {
-        int ans = 1;
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            map<int, int> m, cnt;
-            for (int j = i; j < n; j++) {
-                if (m.find(nums[j]) != m.end()) {
-                    cnt[m[nums[j]]]--;
-                    if (!cnt[m[nums[j]]]) cnt.erase(m[nums[j]]);
+    
+    int getLength(vector<int> &nums)
+    {   int res = 1;
+                unordered_map<int, int> mp;
+            unordered_map<int, int> fp;
+        for (int i = 0; i < nums.size(); i++)
+        {
+
+            for (int j = i; j < nums.size(); j++)
+            {
+                int before = mp[nums[j]];
+                if (before != 0)
+                    fp[before]--;
+                if (fp[before] == 0)
+                {
+                    fp.erase(before);
                 }
-                m[nums[j]]++;
-                cnt[m[nums[j]]]++;
-                if (m.size() == 1) ans = max(ans, j - i + 1);
-                else if (cnt.size() == 2) {
-                    int v1 = -1, v2 = -1;
-                    for (auto &c : cnt) {
-                        if (v1 == -1) v1 = c.first;
-                        v2 = c.first;
+
+                mp[nums[j]]++;
+                if (mp.size() == 1)
+                {
+                    res = max(res, j - i + 1);
+                }
+                int after = mp[nums[j]];
+                fp[after]++;
+                if (fp.size() == 2)
+                {
+                    int val1 = (*fp.begin()).first;
+                    int val3 = -1;
+                    if(val1 %2 == 0) val3 = val1/2;
+                    if (fp.find(val1 * 2) != fp.end() || fp.find(val3) != fp.end())
+                    {
+                        res = max(res, j - i + 1);
                     }
-                    if (v2 == 2 * v1) ans = max(ans, j - i + 1);
                 }
             }
+            mp.clear();
+            fp.clear();
         }
-        return ans;
+        return res;
     }
 };
