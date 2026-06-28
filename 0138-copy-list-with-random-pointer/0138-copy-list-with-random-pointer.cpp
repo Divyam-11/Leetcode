33a5bb0de4439e7d7059f74f6383ed35adcf2512
Neo <83936994+Divyam-11@users.noreply.github.com>
2026-06-28 21:21:52 +0530

    Time: 3 ms (91.54%), Space: 15.2 MB (47.03%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    Node *copyRandomList(Node *head)
+    {
+        unordered_map<Node *, Node *> mp;
+        Node *temp = head;
+        while (temp)
+        {
+            mp[temp] = new Node(temp->val);
+            temp = temp->next;
+        }
+        temp = head;
+        while (temp)
+        {
+            mp[temp]->random = mp[temp->random];
+            mp[temp]->next = mp[temp->next];
+            temp = temp->next;
+        }
+        return mp[head];
+    }
+};
