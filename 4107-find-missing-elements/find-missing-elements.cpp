fb4b4b6368ca551509c094a0dc470133c536024a
Neo <83936994+Divyam-11@users.noreply.github.com>
2026-08-04 09:25:05 +0530

    Time: 4 ms (35.49%) | Memory: 33.8 MB (17.56%) - LeetSync

---
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    vector<int> findMissingElements(vector<int> &nums)
+    {
+        int smallest = INT_MAX;
+        int largest = INT_MIN;
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            largest = max(largest, nums[i]);
+            smallest = min(smallest, nums[i]);
+            mp[nums[i]]++;
+        }
+        vector<int> res;
+        for (int i = smallest; i <= largest; i++)
+        {
+            if (mp.find(i) == mp.end())
+            {
+                res.push_back(i);
+            }
+        }
+        return res;
+    }
+};
