c603cc63f58aaa746b7c2944a742b7b3b20d5b3f
Neo <83936994+Divyam-11@users.noreply.github.com>
2026-08-05 11:45:09 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (56.91%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,21 +1,35 @@
-class Solution {
-    private:
-    void dfs(vector<vector<int>> & image, int sr,int sc ,int color,int og_pixel){
-        if(sr < 0 || sr>=image.size() || sc < 0 || sr >= image[0].size()) return ;
-        if(image[sr][sc] == color) return;
-        if(image[sr][sc] != og_pixel ) return;
-        image[sr][sc] = color;
-        dfs(image,sr+1,sc,color,og_pixel);
-        dfs(image,sr,sc+1,color,og_pixel);
-        dfs(image,sr-1,sc,color,og_pixel);
-        dfs(image,sr,sc-1,color,og_pixel);
-        return;
-
-    }
+class Solution
+{
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int og_pixel = image[sr][sc];
-        dfs(image,sr,sc,color,og_pixel);
+    vector<int> rows = {0, -1, 0, 1};
+    vector<int> cols = {-1, 0, 1, 0};
+    vector<vector<int>> floodFill(vector<vector<int>> &image, int sr, int sc, int color)
+    {
+        int m = image.size();
+        int n = image[0].size();
+        queue<pair<int, int>> q;
+        int src_color = image[sr][sc];
+        q.push({sr, sc});
+        image[sr][sc] = color;
+        if(src_color == color) return image;
+        while (!q.empty())
+        {
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+            for (int i = 0; i < 4; i++)
+            {
+                int newRow = x + rows[i];
+                int newCol = y + cols[i];
+                if (newRow < 0 || newCol < 0 || newRow >= m || newCol >= n)
+                    continue;
+                if (image[newRow][newCol] == src_color)
+                {
+                    image[newRow][newCol] = color;
+                    q.push({newRow, newCol});
+                }
+            }
+        }
         return image;
     }
 };
