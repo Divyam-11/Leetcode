07f55af58833444121e1635b780070ab3df73aa1
Neo <83936994+Divyam-11@users.noreply.github.com>
2026-08-05 22:31:17 +0530

    Time: 117 ms (11.09%) | Memory: 120.6 MB (36.12%) - LeetSync

--- 1876-map-of-highest-peak/map-of-highest-peak.cpp
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -1,42 +1,47 @@
 class Solution
 {
 public:
-    vector<vector<int>> highestPeak(vector<vector<int>> &isWater)
+    vector<int> rows = {0, -1, 0, 1};
+    vector<int> cols = {-1, 0, 1, 0};
+    vector<vector<int>> highestPeak(vector<vector<int>> &mat)
     {
-        vector<int> rows = {0, 0, 1, -1};
-        vector<int> columns = {1, -1, 0, 0};
-        int m = isWater.size();
-        int n = isWater[0].size();
-        vector<vector<int>> result(m, vector<int>(n, 0));
-        queue<pair<int, int>> q;
+        int m = mat.size();
+        int n = mat[0].size();
+        queue<pair<int, pair<int, int>>> q;
+        vector<vector<int>> visited(m, vector<int>(n, INT_MAX));
         for (int i = 0; i < m; i++)
         {
             for (int j = 0; j < n; j++)
             {
-                if (isWater[i][j] == 1)
-                    q.push({i, j});
-                else
-                    result[i][j] = -1;
+                mat[i][j] = !mat[i][j];
+                if (mat[i][j] == 0)
+                {
+                    q.push({0, {i, j}});
+                    visited[i][j] = 0;
+                }
             }
         }
         while (!q.empty())
         {
-            int x = q.front().first;
-            int y = q.front().second;
+            int dis = q.front().first;
+            int x = q.front().second.first;
+            int y = q.front().second.second;
             q.pop();
-
             for (int i = 0; i < 4; i++)
             {
-                int row = x + rows[i];
-                int col = y + columns[i];
-                int height = result[x][y];
-                if (row < 0 || col < 0 || row >= m || col >= n || isWater[row][col] == 1 || result[row][col] != -1)
+                int newRow = x + rows[i];
+                int newCol = y + cols[i];
+                if (newRow < 0 || newCol < 0 || newRow >= m || newCol >= n || mat[newRow][newCol] == 0)
+                {
                     continue;
-
-                result[row][col] = height + 1;
-                q.push({row, col});
+                }
+                if (visited[newRow][newCol] > dis + 1)
+                {
+                    visited[newRow][newCol] = dis + 1;
+                    q.push({dis + 1, {newRow, newCol}});
+                }
             }
         }
-        return result;
+        return visited;
     }
 };
