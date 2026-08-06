6add0bbf373bd6da48fcb6906c78d0c27f966615
Neo <83936994+Divyam-11@users.noreply.github.com>
2026-08-06 12:15:11 +0530

    Time: 311 ms (26.57%) | Memory: 20.7 MB (80.93%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -25,7 +25,7 @@ public:
         if (!flag)
             return 0;
         set<string> wl(wordList.begin(), wordList.end());
-        
+
         queue<pair<int, string>> q;
         q.push({1, beginWord});
         wl.erase(beginWord);
@@ -52,5 +52,44 @@ public:
             }
         }
         return 0;
+    }
+    void dfs(string word, int cost, int &LL, string &beginWord, string &endWord, set<string> &wl, set<string> &st, vector<string> &key, vector<vector<string>> &result)
+    {
+        if (cost >= LL)
+            return;
+        st.insert(word);
+        for (int i = 0; i < word.size(); i++)
+        {
+            char temp = word[i];
+            for (int j = 0; j < 26; j++)
+            {
+                word[i] = j + 'a';
+                if (wl.find(word) != wl.end())
+                {
+                    if (word == endWord)
+                    {
+                        key.push_back(word);
+                        result.push_back(key);
+                        key.pop_back();
+                        return;
+                    }
+                    key.push_back(word);
+                    dfs(word, cost + 1, LL, beginWord, endWord, wl, st, key, result);
+                    key.pop_back();
+                }
+            }
+            word[i] = temp;
+        }
+        st.erase(word);
+    }
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string> &wordList)
+    {
+        int LL = ladderLength(beginWord, endWord, wordList);
+        set<string> wl(wordList.begin(), wordList.end());
+        set<string> st;
+        vector<string> key;
+        vector<vector<string>> result;
+        dfs(beginWord, 0, LL, beginWord, endWord, wl, st, key, result);
+        return result;
     }
 };
