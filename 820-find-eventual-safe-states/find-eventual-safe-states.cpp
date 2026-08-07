cd7db2df81b858cdd4cea30f9210fd9ab92453ed
Neo <83936994+Divyam-11@users.noreply.github.com>
2026-08-07 19:25:14 +0530

    Time: 58 ms (17.94%) | Memory: 66.1 MB (52.71%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,32 +1,28 @@
 class Solution
 {
 public:
-    vector<int> eventualSafeNodes(vector<vector<int>> &graph)
+    vector<int> topoSort(int n, vector<vector<int>> &adjList)
     {
-        vector<vector<int>> adjList(graph.size());
-        vector<int> inDegree(graph.size());
-        for (int i = 0; i < graph.size(); i++)
+        vector<int> inDegree(n);
+        for (int i = 0; i < adjList.size(); i++)
         {
-            for (auto it : graph[i])
+            for (int j = 0; j < adjList[i].size(); j++)
             {
-                adjList[it].push_back(i);
-                inDegree[i]++;
+                inDegree[adjList[i][j]]++;
             }
         }
-
+        vector<int> topo;
         queue<int> q;
-        vector<int> safeNodes;
-        for (int i = 0; i < graph.size(); i++)
+        for (int i = 0; i < n; i++)
         {
-
             if (inDegree[i] == 0)
                 q.push(i);
         }
         while (!q.empty())
         {
             int node = q.front();
+            topo.push_back(node);
             q.pop();
-            safeNodes.push_back(node);
             for (auto it : adjList[node])
             {
                 inDegree[it]--;
@@ -34,7 +30,22 @@ public:
                     q.push(it);
             }
         }
-        sort(safeNodes.begin(), safeNodes.end());
-        return safeNodes;
+        return topo;
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>> &graph)
+    {
+        vector<vector<int>> adjList(graph.size());
+        for (int i = 0; i < graph.size(); i++)
+        {
+            for (int j = 0; j < graph[i].size(); j++)
+            {
+                int u = i;
+                int v = graph[i][j];
+                adjList[v].push_back(u);
+            }
+        }
+        vector<int> topo = topoSort(graph.size(), adjList);
+        sort(topo.begin(), topo.end());
+        return topo;
     }
 };
