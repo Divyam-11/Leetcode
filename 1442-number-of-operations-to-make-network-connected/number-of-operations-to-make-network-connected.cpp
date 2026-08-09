4a831dff20fecbc3d221cfed25759ab309908f71
Neo <83936994+Divyam-11@users.noreply.github.com>
2026-08-09 11:55:49 +0530

    Time: 1 ms (89.53%) | Memory: 42.1 MB (92.56%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,39 +1,67 @@
+class DSU
+{
+public:
+    vector<int> parent, rank;
+    int n;
+    DSU(int n)
+    {
+        parent.resize(n);
+        rank.resize(n);
+        this->n = n;
+        for (int i = 0; i < n; i++)
+        {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+    }
+    int findUParent(int node)
+    {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUParent(parent[node]);
+    }
+    void unionByRank(int u, int v)
+    {
+
+        int pu = findUParent(u);
+        int pv = findUParent(v);
+        if (pu == pv)
+            return;
+        if (rank[pv] == rank[pu])
+        {
+            parent[pv] = pu;
+            rank[pu]++;
+        }
+        else if (rank[pu] < rank[pv])
+        {
+            parent[pu] = pv;
+        }
+        else
+        {
+            parent[pv] = pu;
+        }
+    }
+};
 class Solution
 {
 public:
-    void dfs(int node, vector<vector<int>> &adjList, vector<int> &visited)
-    {
-        visited[node] = 1;
-        for (auto it : adjList[node])
-        {
-            if (!visited[it])
-            {
-                dfs(it, adjList, visited);
-            }
-        }
-    }
     int makeConnected(int n, vector<vector<int>> &connections)
     {
-
-        int edgesCount = connections.size();
-        if (edgesCount < n - 1)
+        if (connections.size() < n - 1)
             return -1;
-        vector<vector<int>> adjList(n);
+        DSU ds(n);
+        int components = 0;
         for (int i = 0; i < connections.size(); i++)
         {
             int u = connections[i][0];
             int v = connections[i][1];
-            adjList[u].push_back(v);
-            adjList[v].push_back(u);
+            ds.unionByRank(u, v);
         }
-        int components = 0;
-        vector<int> visited(n);
         for (int i = 0; i < n; i++)
         {
-            if (!visited[i])
+            if (ds.findUParent(i) == i)
             {
                 components++;
-                dfs(i, adjList, visited);
             }
         }
         return components - 1;
